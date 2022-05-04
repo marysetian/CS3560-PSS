f59777a67388c6f04b08849e9abb4184b0f39afd
AltoInferno <89229186+AltoInferno@users.noreply.github.com>
2022-05-03 20:00:17 -0700

    Update RecurringTask.java

--- src/project1/RecurringTask.java
+++ src/project1/RecurringTask.java
@@ -8,6 +8,8 @@ public class RecurringTask extends Task {
 //    private String type;
 //    private float startTime;
 //    private float duration;
+    //
+    //
 
     private int startDate;  //YYYYMMDD
     private int endDate;    //YYYYMMDD
@@ -20,7 +22,7 @@ public class RecurringTask extends Task {
     * constructor for a Recurring Task
     * @param name   name of Recurring task - must be unique
     * @param type   must be a value from type array
-    * @param startTime  time task begins
+    * @param StartTime  time task begins
     * @param duration   duration of task
     * @param startDate  Date task begins
     * @param endDate    Date task ends
@@ -44,31 +46,43 @@ public class RecurringTask extends Task {
     	System.out.println("\t\t\tCREATE RECURRING TASK");
         System.out.println("--------------------------------------------------------------------------------");
         System.out.println("Enter Task Name: ");
-        //TODO: determine if main method or task method determines validity
     	boolean validName = false;
     	while(!validName) {
         	String inputName = keyboard.nextLine().trim();
-    		if (Schedule.hm.containsKey(inputName)) {
-	    		System.out.println("Invalid name: schedule contains duplicate.\n"
-	    							+ "Retry name entry? 1. Yes 2. No\n");
-	    		int choice = keyboard.nextInt();
-	    		if (choice == 1) {
-	    			continue;
-	    		}
-	    		else {
-	    			keyboard.close();
-	    			return;
-	    		}
+    		if (!Schedule.hm.containsKey(inputName) && inputName.length() <= 30) {
+    			validName = true;
+        		this.setName(inputName);
     		}
-    		validName = true;
-    		this.setName(inputName);
+    		else {
+    			if (Schedule.hm.containsKey(inputName)) {
+    	    		System.out.println("Invalid name: schedule contains duplicate.\n");
+    			}
+    			else if (inputName.length() >= 30) {
+    				System.out.println("Invalid name: name too long.\n");
+    			}
+    			System.out.println("Retry name entry?\n1. Yes \n2. No");
+    			int choice = keyboard.nextInt();
+    			if (choice == 1) {
+    				continue;
+    			}
+    			else {
+    				System.out.println("Task not created, returning to main menu.\n");
+    				keyboard.close();
+    				return;
+    			}
+    		}
     	}
     	boolean setValid = true;
         while(setValid) {
-            System.out.println("\nSelect Type: \n");
-            System.out.println("1. Class \n2. Study \n3. Sleep \n4. "
+            System.out.println("\nSelect Type, or enter 0 to return to menu: \n");
+            System.out.println("0. Exit\n1. Class \n2. Study \n3. Sleep \n4. "
             		+ "Exercise \n5. Work \n6. Meal \n");
     		int choice = keyboard.nextInt();
+    		if (choice == 0) {
+    			createFailed();
+    			keyboard.close();
+    			return;
+    		}
             if (choice == 1){
                 setType(validTypes[0]);
                 setValid = false;}
@@ -91,26 +105,90 @@ public class RecurringTask extends Task {
             	System.out.println("\nInvalid type, please enter again.");
             }
         }
-        System.out.println("Enter Start Time: ");
-        float iStart = (keyboard.nextFloat());
-        setStartTime(iStart);
-        
-        System.out.println("Enter Duration: ");
-        float iDuration = (keyboard.nextFloat());
-        setDuration(iDuration);
-        
-        System.out.println("Enter frequency: ");
-        int iFrequency = (keyboard.nextInt());
-        setFrequency(iFrequency);
-	    
-        System.out.println("Enter start date: ");
-        int iStartDate = (keyboard.nextInt());
-        setStartDate(iStartDate);
-        
-        System.out.println("Enter end date: ");
-        int iEndDate = (keyboard.nextInt());
-        setEndDate(iEndDate);
-        keyboard.close();
+        while(true){
+	        System.out.println("Enter start hour: ");
+	        float startHour = (keyboard.nextFloat());
+	        System.out.println("Enter start minute: ");
+	        float startMinute = (keyboard.nextFloat());
+	        System.out.println("Enter start am or pm: ");
+	        String dayTime = (keyboard.next());
+	        System.out.println(Main.verifyStartTime(startHour, startMinute, dayTime));
+	        if (Main.verifyStartTime(startHour, startMinute, dayTime) != 0) {
+	        	break;
+	        }
+	        System.out.println("Invalid time. retry date entry?\n 1. Yes\n 2. No\n");
+			int choice = keyboard.nextInt();
+			if (choice != 1){
+				createFailed();
+				keyboard.close();
+				return;
+			}
+        }
+        while(true ) {
+	        System.out.println("Enter Duration hour: ");
+	        float durHour = (keyboard.nextFloat());
+	        System.out.println("Enter Duration minute: ");
+	        float durMinute = (keyboard.nextFloat());
+	        System.out.println(Main.verifyDuration(durHour, durMinute));
+	        if (Main.verifyDuration(durHour, durMinute) != 0) {
+	        	break;
+	        }
+	        System.out.println("Invalid duration. retry duration entry?\n 1. Yes\n 2. No\n");
+			int choice = keyboard.nextInt();
+			if (choice != 1){
+				createFailed();
+				keyboard.close();
+				return;
+			}
+        }
+
+        while(true) {
+	        System.out.println("Enter frequency: ");
+	        int iFrequency = (keyboard.nextInt());
+	        System.out.println( Main.verifyFrequency(iFrequency));
+	        if (Main.verifyFrequency(iFrequency)) {
+	        	break;
+	        }
+	        System.out.println("Invalid frequency. retry frequency entry?\n 1. Yes\n 2. No\n");
+			int choice = keyboard.nextInt();
+			if (choice != 1){
+				createFailed();
+				keyboard.close();
+				return;
+			}
+        }
+        int iStartDate;
+		while(true) {
+	        System.out.println("Enter start date in format YYYYMMDD: ");
+	        iStartDate = (keyboard.nextInt());
+	        System.out.println(Main.verifyDate(iStartDate));
+	        if (Main.verifyFrequency(iStartDate)) {
+	        	break;
+	        }
+	        System.out.println("Invalid start date. retry start date entry?\n 1. Yes\n 2. No\n");
+			int choice = keyboard.nextInt();
+			if (choice != 1){
+				createFailed();
+				keyboard.close();
+				return;
+			}
+		}
+		while(true) {
+	        System.out.println("Enter end date: ");
+	        int iEndDate = (keyboard.nextInt());
+	        System.out.println(Main.verifyEndDate(iStartDate,iEndDate));
+	        keyboard.close();
+	        if (Main.verifyEndDate(iStartDate,iEndDate)) {
+	        	break;
+	        }
+	        System.out.println("Invalid frequency. retry frequency entry?\n 1. Yes\n 2. No\n");
+			int choice = keyboard.nextInt();
+			if (choice != 1){
+				createFailed();
+				keyboard.close();
+				return;
+			}
+		}
     }
     /**
     * print all attributes of Task
@@ -122,70 +200,235 @@ public class RecurringTask extends Task {
     }
 
     /**
-    * deletes task and corresponding anti-task
-    * */
-    public void delete(){}
-
-    /**
     * displays a menu for a user to edit any task attributes except "available"
     * */
     public void edit(){
-    Scanner keyboard = new Scanner(System.in);
-    boolean setValid = true;
-    while(setValid) {
-        System.out.println("\nSelect parameter to edit: \n");
-        System.out.println("1. Name \n2. Type \n3. Start Time \n4. Duration"
-        		+ "Start date \n5. End date \n6. Frequency \n");
-		int choice = keyboard.nextInt();
-		//switch for parameter to edit
-        if (choice == 1){
-            setType(validTypes[0]);
-            setValid = false;}
-        if (choice == 2){
-            setType(validTypes[1]);
-            setValid = false;}
-        if (choice == 3){
-            setType(validTypes[2]);
-            setValid = false;}
-        if (choice == 4){
-            setType(validTypes[3]);
-            setValid = false;}
-        if (choice == 5){
-            setType(validTypes[4]);
-            setValid = false;}
-        if (choice == 6){
-            setType(validTypes[5]);
-            setValid = false;}
-        if (setValid == true) {
-        	System.out.println("\nInvalid parameter, please enter again.");
-        	}
-    	}
+	    Scanner keyboard = new Scanner(System.in);
+	    boolean setValid = true;
+	    while(setValid) {
+	        System.out.println("\nSelect parameter to edit or 0 to exit: \n");
+	        System.out.println("0. Exit\n1. Name \n2. Type \n3. Start Time \n4. Duration"
+	        		+"\n5. Start date \n6. End date \n7. Frequency \n");
+			int select = keyboard.nextInt();
+			//switch for parameter to edit
+			if (select == 0){
+				editFailed();
+				keyboard.close();
+				return;
+			}
+	        if (select == 1){
+	        	System.out.println("Enter Task Name: ");
+	        	boolean validName = false;
+	        	while(!validName) {
+	            	String inputName = keyboard.nextLine().trim();
+	        		if (!Schedule.hm.containsKey(inputName) && inputName.length() <= 30) {
+	        			validName = true;
+	            		this.setName(inputName);
+	            		editSuccess();
+	            		keyboard.close();
+	            		return;
+	        		}
+	        		else {
+	        			if (Schedule.hm.containsKey(inputName)) {
+	        	    		System.out.println("Invalid name: schedule contains duplicate.\n");
+	        			}
+	        			else if (inputName.length() >= 30) {
+	        				System.out.println("Invalid name: name too long.\n");
+	        			}
+	        			System.out.println("Retry name entry?\n1. Yes \n2. No");
+	        			int choice = keyboard.nextInt();
+	        			if (choice == 1) {
+	        				continue;
+	        			}
+	        			else {
+	        				editFailed();
+	        				keyboard.close();
+	        				return;
+	        			}
+	        		}
+	        	}
+	        }
+	        if (select == 2){
+	        	while(setValid) {
+	                System.out.println("\nSelect Type, or enter 0 to return to menu: \n");
+	                System.out.println("0. Exit\n1. Class \n2. Study \n3. Sleep \n4. "
+	                		+ "Exercise \n5. Work \n6. Meal \n");
+	        		int choice = keyboard.nextInt();
+	        		if (choice == 0) {
+	        			editFailed();
+	        			keyboard.close();
+	        			return;
+	        		}
+	                if (choice == 1){
+	                    setType(validTypes[0]);
+	                    setValid = false;}
+	                if (choice == 2){
+	                    setType(validTypes[1]);
+	                    setValid = false;}
+	                if (choice == 3){
+	                    setType(validTypes[2]);
+	                    setValid = false;}
+	                if (choice == 4){
+	                    setType(validTypes[3]);
+	                    setValid = false;}
+	                if (choice == 5){
+	                    setType(validTypes[4]);
+	                    setValid = false;}
+	                if (choice == 6){
+	                    setType(validTypes[5]);
+	                    setValid = false;}
+	                if (setValid == true) {
+	                	System.out.println("\nInvalid type, please enter again.");
+	                }
+	        	}
+	        	editSuccess();
+	        	keyboard.close();
+	        	return;
+	        }
+	        if (select == 3){
+	        	while(true){
+	    	        System.out.println("Enter start hour: ");
+	    	        float startHour = (keyboard.nextFloat());
+	    	        System.out.println("Enter start minute: ");
+	    	        float startMinute = (keyboard.nextFloat());
+	    	        System.out.println("Enter start am or pm: ");
+	    	        String dayTime = (keyboard.next());
+	    	        System.out.println(Main.verifyStartTime(startHour, startMinute, dayTime));
+	    	        if (Main.verifyStartTime(startHour, startMinute, dayTime) != 0) {
+	    	        	editSuccess();
+	    	        	keyboard.close();
+	    	        	return;
+	    	        }
+	    	        System.out.println("Invalid time. retry date entry?\n 1. Yes\n 2. No\n");
+	    			int choice = keyboard.nextInt();
+	    			if (choice != 1){
+	    				editFailed();
+	    				keyboard.close();
+	    				return;
+	    			}
+	        	}
+	        }
+	        if (select == 4){
+	        	while(true ) {
+	    	        System.out.println("Enter Duration hour: ");
+	    	        float durHour = (keyboard.nextFloat());
+	    	        System.out.println("Enter Duration minute: ");
+	    	        float durMinute = (keyboard.nextFloat());
+	    	        System.out.println(Main.verifyDuration(durHour, durMinute));
+	    	        if (Main.verifyDuration(durHour, durMinute) != 0) {
+	    	        	editSuccess();
+	    	        	keyboard.close();
+	    	        	return;
+	    	        }
+	    	        System.out.println("Invalid duration. retry duration entry?\n 1. Yes\n 2. No\n");
+	    			int choice = keyboard.nextInt();
+	    			if (choice != 1){
+	    				editFailed();
+	    				keyboard.close();
+	    				return;
+	    			}
+	            }
+	        }
+	        if (select == 5){
+	    		while(true) {
+	    	        System.out.println("Enter start date in format YYYYMMDD: ");
+	    	        int iStartDate = (keyboard.nextInt());
+	    	        System.out.println(Main.verifyDate(iStartDate));
+	    	        if (Main.verifyFrequency(iStartDate)) {
+	    	        	editSuccess();
+	    	        	keyboard.close();
+	    	        	return;
+	    	        }
+	    	        System.out.println("Invalid start date. retry start date entry?\n 1. Yes\n 2. No\n");
+	    			int choice = keyboard.nextInt();
+	    			if (choice != 1){
+	    				editFailed();
+	    				keyboard.close();
+	    				return;
+	    			}
+	    		}
+	        }
+	        if (select == 6){
+	        	int iStartDate = startDate;
+	        	while(true) {
+	    	        System.out.println("Enter end date: ");
+	    	        int iEndDate = (keyboard.nextInt());
+	    	        System.out.println(Main.verifyEndDate(iStartDate,iEndDate));
+	    	        keyboard.close();
+	    	        if (Main.verifyEndDate(iStartDate,iEndDate)) {
+	    	        	break;
+	    	        }
+	    	        System.out.println("Invalid frequency. retry frequency entry?\n 1. Yes\n 2. No\n");
+	    			int choice = keyboard.nextInt();
+	    			if (choice != 1){
+	    				editFailed();
+	    				keyboard.close();
+	    				return;
+	    			}
+	    		}
+	        }
+	        if (select == 7){
+	        	while(true) {
+	    	        System.out.println("Enter frequency: ");
+	    	        int iFrequency = (keyboard.nextInt());
+	    	        System.out.println( Main.verifyFrequency(iFrequency));
+	    	        if (Main.verifyFrequency(iFrequency)) {
+	    	        	editSuccess();
+	    	        	keyboard.close();
+	    	        	return;
+	    	        }
+	    	        System.out.println("Invalid frequency. retry frequency entry?\n 1. Yes\n 2. No\n");
+	    			int choice = keyboard.nextInt();
+	    			if (choice != 1){
+	    				editFailed();
+	    				keyboard.close();
+	    				return;
+	    			}
+	        	}
+	        }
+	        if (setValid == true) {
+	        	System.out.println("\nInvalid parameter, please enter again.");
+	        }
+	    }
+	}
+    
+    private void createFailed() {
+    	System.out.println("Task not created, returning to main menu.\n");
     }
-    private void setName(String name) {
+    
+    private void editFailed() {
+    	System.out.println("Task not edited, returning to main menu.\n");
+    }
+    
+    private void editSuccess() {
+    	System.out.println("Task edit successful, returning to main menu.\n");
+    }
+    
+    public void setName(String name) {
     	this.name = name;
     }
     
-    private void setType(String type) {
+    public void setType(String type) {
     	this.type = type;
     }
     
-    private void setStartTime(float startTime) {
+    public void setStartTime(float startTime) {
     	this.startTime = startTime;
     }
-    
-    private void setDuration(float duration) {
+
+    public void setDuration(float duration) {
     	this.duration = duration;
     }
     
-    private void setStartDate(int startDate) {
+    public void setStartDate(int startDate) {
     	this.startDate = startDate;
     }
     
-    private void setEndDate(int endDate) {
+    public void setEndDate(int endDate) {
     	this.endDate = endDate;
     }
     
-    private void setFrequency(int frequency) {
+    public void setFrequency(int frequency) {
     	this.frequency = frequency;
     }
 
