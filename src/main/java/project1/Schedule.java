c4fbaab6ec8beaf45d8e2d60fff1175ebd51ed27
John Smith <thaolinda2727@gmail.com>
2022-05-16 15:52:09 -0700

    Updated Schedule.java to check recurring and AntiTasks

--- src/main/java/project1/Schedule.java
+++ src/main/java/project1/Schedule.java
@@ -28,7 +28,6 @@ public class Schedule {
      * verify file category is valid when reading from a file
      * */
 
-
     //write schedule to json file for a week
     public static void writeWeeklyToFile( String filename, int givenDate) throws ParseException {
 
@@ -48,7 +47,7 @@ public class Schedule {
 
         //new treeMap to hold the new tasks that fit within the range
         // treeMap to hold the exported data from hm to organize by time - for WEEKLY
-        Map<Float, Task> weeklyTasks = new TreeMap<>();
+        Map<Double, Task> weeklyTasks = new TreeMap<>();
 
         for (Map.Entry mapElement : hm.entrySet()) {
             String key = (String) mapElement.getKey();
@@ -63,7 +62,7 @@ public class Schedule {
                     String date = String.valueOf(tempTransient.getDate());
                     String time = String.valueOf(tempTransient.getStartTime());
 
-                    Float weeklyKey = Float.parseFloat(date + time);
+                    Double weeklyKey = Double.parseDouble(date + time);
                     weeklyTasks.put(weeklyKey, tempTransient);
                 }
 
@@ -73,23 +72,52 @@ public class Schedule {
                 //temp recurring task
                 RecurringTask tempRecurring = (RecurringTask) Schedule.hm.get(key);
 
-                //task date lies within the given start date and +7 days (range for the week)
-                if (givenDate <= tempRecurring.getStartDate() && tempRecurring.getStartDate() <= intEndDate) {
+                ArrayList<Integer> arrList;
 
-                        String date = String.valueOf(tempRecurring.getStartDate());
-                        String time = String.valueOf(tempRecurring.getStartTime());
+                //get the dates of recurring tasks and store into arrayList
+                arrList = returnRecurringDates(givenDate, intEndDate, tempRecurring.getStartDate(), tempRecurring.getEndDate(), tempRecurring.getFrequency());
 
-                        Float weeklyKey = Float.parseFloat(date + time);
-                        weeklyTasks.put(weeklyKey, tempRecurring);
+                for(int i = 0; i < arrList.size(); i++) {
+
+                    //task date lies within the given start date and +7 days (range for the week)
+                    if (givenDate <= arrList.get(i) && arrList.get(i) <= intEndDate) {
+
+                        String date, time;
+                        Double weeklyKey;
+
+                        String recurAntiName = "anti_" + tempRecurring.getName() + "_" + arrList.get(i);
+
+                        if(!hm.containsKey(recurAntiName)) {
+
+                            date = Integer.toString(arrList.get(i));
+                            time = String.valueOf(tempRecurring.getStartTime());
+
+                            String tempKey = date + time;
+
+                            weeklyKey = Double.parseDouble(date + time);
+
+                            System.out.println("DATE: " + date + ", Time: " + time);
+                            System.out.println("tempKey: " + tempKey);
+
+                            System.out.println("DATE: " + arrList.get(i) + ", KEY: " + weeklyKey);
+
+                            weeklyTasks.put(weeklyKey, tempRecurring);
+
+                        } else  {
+                            date = Integer.toString(arrList.get(i));
+                            time = String.valueOf(tempRecurring.getStartTime());
+
+                            weeklyKey = Double.parseDouble(date + time);
+                            System.out.println("UNABLE TO ADD RECURRING TASK BC OF ANTI TASK: " + tempRecurring.getName() + ", with date: " +
+                                                date + ", And KEY: " +  weeklyKey);
+                        }
+                    }
                 }
             }
         }
 
         writeToJSON(filename, weeklyTasks);
-
-        System.out.println("Info added to file: " + filename + "\n");
-
-        }
+    }
 
     public static String incrementDayCalendar(String date, int numOfDaysToIncre) throws ParseException {
 
@@ -104,33 +132,41 @@ public class Schedule {
         return endDate;
     }
 
-    public static ArrayList<Integer> returnRecurringDates(int taskStartDate, int taskEndDate, int frequency) throws ParseException {
+    // returns a list of the recurring occuring in a given range
+    public static ArrayList<Integer> returnRecurringDates(int rangeStart, int rangeEnd, int taskStartDate, int taskEndDate, int frequency) throws ParseException {
         ArrayList<Integer> recurringDays = new ArrayList<Integer>(); // holds all days a recurring task takes place
 
         int recurringDate = taskStartDate;
 
         while(recurringDate <= taskEndDate){
-            if (taskStartDate <= recurringDate && recurringDate <= taskEndDate){
+            if (rangeStart <= recurringDate && recurringDate<= rangeEnd){
                 recurringDays.add(recurringDate);
             }
-            if (frequency == 1){
-                String tempDate = String.valueOf(recurringDate);
-                tempDate = incrementDayCalendar(tempDate, 1);
-                recurringDate = Integer.parseInt(tempDate);
-            }
-            if (frequency == 7){
-                String tempDate = String.valueOf(recurringDate);
-                tempDate = incrementDayCalendar(tempDate, 7);
-                recurringDate = Integer.parseInt(tempDate);
-            }
+            String tempDate = String.valueOf(recurringDate);
+            tempDate = incrementDayCalendar(tempDate, frequency);
+            recurringDate = Integer.parseInt(tempDate);
         }
         return recurringDays;
     }
 
-    public static void writeToJSON(String filename, Map<Float, Task> hm2) throws ParseException {
+    // returns true if a recurring task does occur in given range
+    public static boolean checkRecurringInRange(int rangeStart, int rangeEnd, int taskStartDate, int taskEndDate, int frequency){
+
+        int recurringDate = taskStartDate;
+
+        // is there at least one recurring date within given range?
+        while(recurringDate <= taskEndDate){
+            if (rangeStart <= recurringDate && recurringDate <= rangeEnd){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void writeToJSON(String filename, Map<Double, Task> hm2){
 
         for(Map.Entry mapElement : hm2.entrySet()) {
-            Float key = (Float) mapElement.getKey();
+            Double key = (Double) mapElement.getKey();
 
             if (hm2.get(key).getTaskType().equals("Transient")) {
 
@@ -138,25 +174,15 @@ public class Schedule {
 
                 writeTransient(filename, tempTransient);
 
-            } else if(hm2.get(key).getTaskType().equals("Recurring")){
+            } else if (hm2.get(key).getTaskType().equals("Recurring")) {
 
                 RecurringTask tempRecurr = (RecurringTask) hm2.get(key);
 
-                int arrSize = 7;
-                ArrayList<Integer> arrList  = new ArrayList<Integer>(arrSize);
-
-                //get the dates of recurring tasks and store into arrayList
-                arrList = returnRecurringDates(tempRecurr.getStartDate(), tempRecurr.getEndDate(), tempRecurr.getFrequency());
-
-                writeRecurring(filename, tempRecurr, arrList);
-
-            }
-            else if(hm2.get(key).getTaskType().equals("AntiTask")){
+                writeRecurring(filename, tempRecurr, key);
 
             }
         }
     }
-
 
     public static void writeTransient(String filename, TransientTask ttTask){
 
@@ -164,9 +190,9 @@ public class Schedule {
             JSONObject jsonObject = new JSONObject();
 
             //Inserting key-value pairs into the json object
-            jsonObject.put(ttTask.getName() + ": ", (ttTask.getName() + ", " + ttTask.getType() + ", " +
-                    ttTask.getStartTime() + ", " + ttTask.getDuration() + ", " +
-                    ttTask.getTaskType() + ", " + ttTask.getDate()));
+            jsonObject.put(ttTask.getName() + ": ", ("Task Type: " + ttTask.getDate() +"Name: " + ttTask.getName() + ", Type: " + ttTask.getType() + ", StartTime: " +
+                    ttTask.getStartTime() + ", Duration: " + ttTask.getDuration() + ", Task Type:" +
+                    ttTask.getTaskType()));
 
         try {
 
@@ -181,37 +207,15 @@ public class Schedule {
         System.out.println("JSON file created: "+ jsonObject);
     }
 
-//
-//            try {
-//                FileWriter file = new FileWriter( filename + ".json");
-//                file.write(jsonObject.toJSONString());
-//                file.close();
-//            } catch (IOException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            }
-//            System.out.println("JSON file created: " + jsonObject);
-//    }
-
-
-    public static void writeRecurring(String filename, RecurringTask recurTask, ArrayList arrList){
+    public static void writeRecurring(String filename, RecurringTask recurTask, Double key){
 
         //Creating a JSONObject object
         JSONObject jsonObject = new JSONObject();
 
-        String allDates = "";
-
-        for (int i = 0; i <= 7; i ++) {
-
-            allDates += arrList.get(i) + ", ";
-        }
-
-
         //Inserting key-value pairs into the json object
-        jsonObject.put(recurTask.getName() + ": ", (recurTask.getName() + ", " + recurTask.getType() + ", " +
-                recurTask.getStartTime() + ", " + recurTask.getDuration() + ", " +
-                recurTask.getStartDate() + ", " + recurTask.getEndDate() + ", " + recurTask.getFrequency() + " RECURRING DATES: " + allDates));
-
+        jsonObject.put(recurTask.getName() + ": ", ( "Task Type: " + recurTask.getTaskType() + ", Name: " + recurTask.getName() + ", Type: " + recurTask.getType() +
+                ", StartDate: " + recurTask.getStartDate() + ", StartTime: " + recurTask.getStartTime() + ", Duration: " + recurTask.getDuration() +
+                recurTask.getEndDate() + ", " + recurTask.getFrequency() + ", KEY: " + key ));
 
         try {
 
@@ -224,27 +228,5 @@ public class Schedule {
             e.printStackTrace();
         }
         System.out.println("JSON file created: "+ jsonObject);
-
-
-
     }
-
-    public static void writeAnti(String filename, AntiTask antiTask){
-
-    }
-
-    public static void deleteFile(String filename){
-
-        String fileLoc = "C:\\Users\\thaol\\Desktop\\CS3560 OOP\\CS3560_PSS\\src\\" + filename + ".json";
-
-        File file = new File(fileLoc);
-
-        if (file.delete()) {
-            System.out.println("File deleted successfully");
-        }
-        else {
-            System.out.println("Failed to delete the file");
-        }
-    }
-
 }
